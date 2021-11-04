e3fcf45136add4d0f3b48284957e4c2b2e633cc8
nikita <neket98@bk.ru>
2021-11-04 13:15:22 +0300

    leetcode

--- src/main/kotlin/ru/smirnov/educational/leetcode/L2050ParallelCoursesIII.kt
+++ src/main/kotlin/ru/smirnov/educational/leetcode/L2050ParallelCoursesIII.kt
@@ -1,79 +1,59 @@
 package ru.smirnov.educational.leetcode
 
-import java.util.*
-
 object L2050ParallelCoursesIII {
 
     fun minimumTime(n: Int, relations: Array<IntArray>, time: IntArray): Int = getMaxExecTime(relations, time, n)
 
     fun getMaxExecTime(relations: Array<IntArray>, times: IntArray, n: Int): Int {
-        val enteredItems = mutableSetOf<Int>()
         val relationsNextByPreviousMap = relations.groupBy({ it[0] }, { it[1] })
+        val relationsPreviousByNextMap = relations.groupBy({ it[1] }, { it[0] })
 
-        var max = 0
-        (1..n).forEach { index ->
-            if (!enteredItems.contains(index)) {
-                maxExecTimeForIndexRecurcive(index, times, relationsNextByPreviousMap, enteredItems).apply {
-                    if (max < this) {
-                        max = this
-                    }
-                }
+        //находим все точки, от которых идет начало графа
+        val startGraphsPos = relationsNextByPreviousMap.filter { !relationsPreviousByNextMap.containsKey(it.key) }
+
+        val maxSumPathMap = mutableMapOf<Int, Int>()
+        var totalMaxSum = 0
+        iterator<Set<Int>> {
+            var layerSet = startGraphsPos.keys
+            while (layerSet.isNotEmpty()) {
+                yield(layerSet)
+                layerSet = layerSet.flatMap {
+                    relationsNextByPreviousMap[it] ?: emptyList()
+                }.toSet()
             }
+        }.forEach { layerSet ->
+            layerSet.forEach { point ->
+                val maxSum = times[point - 1] +
+                        (relationsPreviousByNextMap[point]?.maxOfOrNull { maxSumPathMap[it] ?: 0 } ?: 0)
+                if (totalMaxSum < maxSum) totalMaxSum = maxSum
+                maxSumPathMap[point] = maxSum
+            }
+        }
+
+        return maxOf(totalMaxSum, times.maxOrNull() ?: 0)
+    }
+
+    fun IntArray.maxOrNull(): Int? {
+        if (isEmpty()) return null
+        var max = this[0]
+        for (i in 1..lastIndex) {
+            val e = this[i]
+            if (max < e) max = e
         }
         return max
     }
 
-    fun maxExecTimeForIndexRecurcive(
-        index: Int,
-        times: IntArray,
-        relationsNextByPreviousMap: Map<Int, List<Int>>,
-        enteredItems: MutableSet<Int>
-    ): Int =
-        times[index - 1] + run {
-            var max = 0
-            relationsNextByPreviousMap[index]?.forEach {
-                maxExecTimeForIndexRecurcive(it, times, relationsNextByPreviousMap, enteredItems).apply {
-                    if (max < this) {
-                        max = this
-                    }
-                }
-            }
-            max
-        }.apply {
-            enteredItems.add(index)
-        }
-
-    fun maxExecTimeForIndexWhile(
-        index: Int,
-        times: IntArray,
-        relationsNextByPreviousMap: Map<Int, List<Int>>,
-        enteredItems: MutableSet<Int>
-    ): Int {
-        var ci = index
-        var ctime = times[ci - 1]
-        var max = 0
-        val stack = Stack<Pair<Int, Int>>()
-        while (true) {
-            relationsNextByPreviousMap[ci]?.forEach {
-                stack.push(it to ctime + times[it - 1])
-            } ?: run {
-                //one of branch is fulled
-                if (max < ctime) {
-                    max = ctime
-                }
-                ctime = 0
-            }
-            enteredItems.add(ci)
-            if (stack.isEmpty()) {
-                break
-            } else {
-                stack.pop().apply {
-                    ci = this.first
-                    ctime = this.second
-                }
+    inline fun <T, R : Comparable<R>> Iterable<T>.maxOfOrNull(selector: (T) -> R): R? {
+        val iterator = iterator()
+        if (!iterator.hasNext()) return null
+        var maxValue = selector(iterator.next())
+        while (iterator.hasNext()) {
+            val v = selector(iterator.next())
+            if (maxValue < v) {
+                maxValue = v
             }
         }
-
-        return max
+        return maxValue
     }
+
 }
