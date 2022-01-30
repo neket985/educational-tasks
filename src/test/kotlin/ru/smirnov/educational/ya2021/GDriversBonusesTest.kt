c7fdb1af0fdf33eddc9df0f3a940a8f4eddac52c
smirnov <smirnov.n@olimp.bet>
2022-01-30 16:55:42 +0300

    new test

--- src/test/kotlin/ru/smirnov/educational/ya2021/GDriversBonusesTest.kt
+++ src/test/kotlin/ru/smirnov/educational/ya2021/GDriversBonusesTest.kt
@@ -14,7 +14,6 @@ internal class GDriversBonusesTest {
             Duration.ofSeconds(1),
             DataAmount.ofMega(64),
 
-            gDriversTestAssert(listOf(), 0),
             gDriversTestAssert(listOf(10), 500),
             gDriversTestAssert(listOf(1, 2, 3, 4), 5000),
             gDriversTestAssert(listOf(4, 3, 2, 1), 5000),
@@ -23,6 +22,7 @@ internal class GDriversBonusesTest {
             gDriversTestAssert(listOf(1, 2, 3, 3, 3, 3, 1), 5500),
             gDriversTestAssert(listOf(1, 2, 3, 3, 3, 3, 10), 5500),
             gDriversTestAssert(listOf(1, 10, 20, 15, 14, 13, 10), 18 * 500),
+            gDriversTestAssert((0..19997).map { (it % 3) + 1 }, 6666 * 3000),
         )
     }
 
